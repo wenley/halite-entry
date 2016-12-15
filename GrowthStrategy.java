56d3f8afcfef51034e1bc2d0484fe5e45c10b020
Wenley Tong <wenley@squareup.com>
2016-12-14 21:33:16 -0500

    Accumulate recommendations and add close enemy heuristic.

--- GrowthStrategy.java
+++ GrowthStrategy.java
@@ -20,6 +20,8 @@ public class GrowthStrategy {
   }
 
   public void computeRecommendedMoves() {
+    PlayerAveragePositions playerAveragePositions = new PlayerAveragePositions(gameMap, playerCount);
+
     for (int y = 0; y < gameMap.height; y++) {
       for (int x = 0; x < gameMap.width; x++) {
         Location myLocation = new Location(x, y);
@@ -28,29 +30,48 @@ public class GrowthStrategy {
         // Bulk up weak sites
         // Vulnerable to invaders; want to move losers towards invaders
         if (site.strength < site.production * 5) {
-          insert(x, y, Direction.STILL, 1.0);
+          insertAdd(x, y, Direction.STILL, 1.0);
         }
 
         // Move towards neutral territory when you can take it over
         for (Direction direction : Direction.CARDINALS) {
           Site otherSite = gameMap.getSite(gameMap.getLocation(myLocation, direction));
           if (otherSite.owner == 0 && otherSite.strength < site.strength) {
-            insert(x, y, direction, 1.0);
+            insertAdd(x, y, direction, 1.0);
           }
         }
 
         // When surrounded by enemies, move towards their center
         long numAdjacentEnemies = Arrays.stream(Direction.CARDINALS)
-          .map(direction -> gameMap.getSite(gameMap.getLocation(gameMap.getLocation(myLocation, direction), direction)))
+          .map(direction -> {
+            Location twoAway = gameMap.getLocation(gameMap.getLocation(myLocation, direction), direction);
+            return gameMap.getSite(twoAway);
+          })
           .map(otherSite -> otherSite.owner)
           .filter(owner -> owner != 0 && owner != myID)
           .count();
+        if (numAdjacentEnemies > 0) {
+          Direction awayFromSelf = gameMap.moveTowards(playerAveragePositions.positionForOwner(myID).getLocation(), myLocation);
+          Direction towardsEnemy = playerAveragePositions.positions().stream()
+            .filter(entry -> entry.getKey() != myID)
+            .map(Map.Entry::getValue)
+            .map(PlayerAveragePositions.Position::getLocation)
+            .min(myLocation.comparingByDistance())
+            .map(location -> gameMap.moveTowards(myLocation, location))
+            .orElse(awayFromSelf);
+
+          insertAdd(x, y, towardsEnemy, numAdjacentEnemies);
+        }
       }
     }
   }
 
-  public void insert(int x, int y, Direction direction, double value) {
-    moveMapFor(x, y).put(direction, value);
+  private void insertAdd(int x, int y, Direction direction, double value) {
+    Map<Direction, Double> map = moveMapFor(x, y);
+    if (!map.containsKey(direction)) {
+      map.put(direction, 0.0);
+    }
+    map.put(direction, map.get(direction) + value);
   }
 
   public Map<Direction, Double> moveMapFor(int x, int y) {
