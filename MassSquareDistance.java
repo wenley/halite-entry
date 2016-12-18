1084f40b1123c89b1ac29eb06233537dfd303eee
Wenley Tong <wenley@squareup.com>
2016-12-18 17:27:49 -0500

    Implement MassSquareDistance.

--- MassSquareDistance.java
+++ MassSquareDistance.java
@@ -10,7 +10,20 @@ public class MassSquareDistance implements Function<Location, Long> {
   }
 
   @Override
-  public Long apply(Location location) {
-    return (long) 0;
+  public Long apply(Location center) {
+    long value = 0;
+
+    for (int y = 0; y < gameMap.height; y++) {
+      for (int x = 0; x < gameMap.width; x++) {
+        Location cell = new Location(x, y);
+        Site site = gameMap.getSite(cell);
+
+        if (site.owner == owner) {
+          value += site.strength * gameMap.squareDistanceBetween(center, cell);
+        }
+      }
+    }
+
+    return value;
   }
 }
